10263c19ae9a85ba7564ff26b10d795beb3246a8
kamrulcoder <kamrulcoder@gmail.com>
2023-01-25 09:14:44 +0600

    cors change value

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+    origin: ["http://localhost:3000", "https://genuine-panda-f8ff78.netlify.app/"],
     credentials: true,
   })
 );
